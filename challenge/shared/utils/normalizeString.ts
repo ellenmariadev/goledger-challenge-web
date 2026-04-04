3a874e12498e2b86175e70344db7800370d81fb3
ellenmariadev <ellenmariadev@gmail.com>
2026-04-04 15:27:01 -0300

    refactor: change normalizeString to handle portuguese alphabet

--- challenge/shared/utils/normalizeString.ts
+++ challenge/shared/utils/normalizeString.ts
@@ -1,3 +1,7 @@
 export function normalizeString(value: string): string {
-  return value.trim().toLowerCase();
+  return value
+    .trim()
+    .toLowerCase()
+    .normalize("NFD")
+    .replace(/\p{Diacritic}/gu, "");
 }
